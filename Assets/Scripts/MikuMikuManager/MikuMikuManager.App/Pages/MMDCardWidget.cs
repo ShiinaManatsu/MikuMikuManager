494c39da1ef1dbc2eee43b288cd2dbe826bf6a58
Shiina Manatus <whitesky@live.cn>
2020-04-19 00:33:28 +0800

    Update card style

--- Assets/Scripts/MikuMikuManager/MikuMikuManager.App/Pages/MMDCardWidget.cs
+++ Assets/Scripts/MikuMikuManager/MikuMikuManager.App/Pages/MMDCardWidget.cs
@@ -43,38 +43,56 @@ namespace MikuMikuManager.App
             var path = $"{widget.MMDObject.FileName}.png";
             var buttonGroup = new Positioned(
                 bottom: 0,
-                    child: new Container(
-                        decoration: new BoxDecoration(color: Colors.white70),
-                        child: new ButtonBar(
-                            alignment: MainAxisAlignment.end,
-                            mainAxisSize: MainAxisSize.max,
+                right: 0,
+                child: new Container(
+                    color:Colors.white30,
+                    height: 50,
+                    child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisSize: MainAxisSize.max,
                         children: new List<Widget>
                         {
-                            new IconButton(
-                                icon:new Icon(isFavored?Icons.favorite:Icons.favorite_border,
-                                color:isFavored?Colors.pinkAccent:null),
-                                onPressed:()=> widget.MMDObject.IsFavored.Value=!widget.MMDObject.IsFavored.Value
-                            ),
-                            new IconButton(
-                                icon:new Icon(Icons.delete),
-                                onPressed:()=>{ }
-                            ),
-                            new IconButton(
-                                icon:new Icon(Icons.open_in_browser),
-                                onPressed:()=>System.Diagnostics.Process.Start(path.Remove(path.LastIndexOf("\\")))
-                            )
+                            new AspectRatio(
+                                    child:
+                                    new FlatButton(
+                                        child:new Icon(isFavored?Icons.favorite:Icons.favorite_border,
+                                        color:isFavored?Colors.pinkAccent:null),
+                                        onPressed:()=> widget.MMDObject.IsFavored.Value=!widget.MMDObject.IsFavored.Value
+                                    )
+                                ),
+                            new AspectRatio(
+                                    child:
+                                    new FlatButton(
+                                        child:new Icon(Icons.delete),
+                                        onPressed:()=>{
+
+                                        }
+                                    )
+                                ),
+                            new AspectRatio(
+                                    child:
+                                    new FlatButton(
+                                        child:new Icon(Icons.open_in_browser),
+                                        onPressed:()=>System.Diagnostics.Process.Start(path.Remove(path.LastIndexOf("\\")))
+                                    )
+                                )
                         }
                     )
-                        )
-                ); ; ;
+                )
+            ); ; ;
 
-            var card = File.Exists(path) ? new Card(
-                    color: Colors.amber,
-                    child: Image.memory(File.ReadAllBytes(path), fit: BoxFit.cover)
-                ) : new Card(
+            var card = new GestureDetector(
+                    child: File.Exists(path) ? new Card(
                         color: Colors.amber,
-                        child: new Center(child: new Icon(Icons.priority_high))
-                    );
+                        child: Image.memory(File.ReadAllBytes(path), fit: BoxFit.cover)) :
+                            new Card(
+                                color: Colors.amber,
+                                child: new Center(child: new Icon(Icons.priority_high))
+                            )
+                    ,
+                    onTap: () => ShowContextMenu(context)
+                );
 
             var stack = new Stack(
                     children: new List<Widget>
@@ -85,6 +103,33 @@ namespace MikuMikuManager.App
                 );
 
             return stack;
+        }
+
+        private void ShowContextMenu(BuildContext context)
+        {
+            if (UIClickProperty.IsMRBClicked)
+            {
+                PopupMenuUtils.showMenu(
+                    context: context,
+                    position: RelativeRect.fromLTRB(Input.mousePosition.x, Screen.height - Input.mousePosition.y, Screen.width - Input.mousePosition.x, Input.mousePosition.y),
+                    items: new List<PopupMenuEntry<Widget>>
+                    {
+                                                new PopupMenuItem<Widget>(
+                                                        child:new Text("Test Menu A")
+                                                    ),
+                                                new PopupMenuItem<Widget>(
+                                                        child:new Text("Test Menu B")
+                                                    ),
+                                                new PopupMenuItem<Widget>(
+                                                        child:new Text("Test Menu C")
+                                                    ),
+                                                new PopupMenuItem<Widget>(
+                                                        child:new Text("Test Menu D")
+                                                    )
+                    },
+                    initialValue: null
+                );
+            }
         }
 
         protected override bool wantKeepAlive => true;
