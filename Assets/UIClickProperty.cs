4548a7f18ea6dbc9b8fb51da43c6c048efba5eae
Shiina Manatus <whitesky@live.cn>
2020-04-19 00:38:46 +0800

    Create UIClickProperty.cs
    Solve that both fluter and unirx.triggers doesn't have mouse right button down event

---
+++ Assets/UIClickProperty.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+public class UIClickProperty : MonoBehaviour
+{
+    public static bool IsMRBClicked { get; set; } = false;
+    public ReactiveProperty<bool> isMRBClicked;
+
+    void Start()
+    {
+        isMRBClicked = new ReactiveProperty<bool>(false);
+
+        var t = isMRBClicked
+            .Where(x => x)
+            .Do(x => Debug.Log(x))
+            .Subscribe(x => IsMRBClicked = x);
+
+        var f = isMRBClicked
+            .Where(x => !x)
+            .Delay(TimeSpan.FromSeconds(1))
+            .Do(x => Debug.Log(x))
+            .Subscribe(x => IsMRBClicked = x);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            isMRBClicked.Value = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            isMRBClicked.Value = false;
+        }
+    }
+}
