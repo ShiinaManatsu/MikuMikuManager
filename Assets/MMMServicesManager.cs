7bc1f25258d8c3113e85b80dc8fb7b47de2971f9
Shiina Manatus <whitesky@live.cn>
2020-04-19 00:31:36 +0800

    Wait for loading services

--- Assets/MMMServicesManager.cs
+++ Assets/MMMServicesManager.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using System;
+using MikuMikuManager.App;
 
 namespace MikuMikuManager.Services
 {
     public class MMMServicesManager : MonoBehaviour
     {
+        public GameObject panel;
         void Start()
         {
+            Observable
+                .Return(true)
+                .Delay(TimeSpan.FromSeconds(3))
+                .Subscribe(_ => panel.AddComponent<MMMFlutterApp>());
         }
     }
 
