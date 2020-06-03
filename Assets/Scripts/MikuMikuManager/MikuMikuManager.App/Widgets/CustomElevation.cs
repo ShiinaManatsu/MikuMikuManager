75b2eabf82c52bc40fe6fd37ac03d73e5473e397
Shiina Manatus <whitesky@live.cn>
2020-06-03 22:19:38 +0800

    New widget to add shadow to child

---
+++ Assets/Scripts/MikuMikuManager/MikuMikuManager.App/Widgets/CustomElevation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.painting;
+using Unity.UIWidgets.ui;
+using Unity.UIWidgets.widgets;
+
+namespace MikuMikuManager.App
+{
+    class CustomElevation : StatelessWidget
+    {
+        private Widget Child { get; set; }
+        private float Height { get; set; }
+        private EdgeInsets Padding { get; set; }
+        private EdgeInsets Margin { get; set; }
+
+        public CustomElevation(Widget child, float height = 10, EdgeInsets padding = null, EdgeInsets margin = null)
+        {
+            Child = child;
+            Height = height;
+            Padding = padding;
+            Margin = margin;
+        }
+
+
+        public override Widget build(BuildContext context)
+        {
+            return new Container(
+              decoration: new BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(Height / 2)),
+                boxShadow: new List<BoxShadow>
+                {
+                    new BoxShadow(
+                    color: Colors.black.withOpacity(0.2f),
+                    blurRadius: Height / 5,
+                    offset:new Offset(0, 0)
+                  )
+                }
+              ),
+              child: Child,
+              margin: Margin is null ? EdgeInsets.all(Height / 5) : Margin,
+              padding: Padding
+            );
+        }
+    }
+}
